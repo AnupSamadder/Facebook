870838d70060a8c52a0d9d9d1f203a68c412a028
Anup Samadder <anup.samadder07@gmail.com>
2021-05-25 14:01:10 +0600

    first commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Login.css';
+import facebook from '../../images/facebook.png';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <div className="login">
+            <img src={facebook} alt="" className="login-logo" />
+            <div className="login-container">
+                <h3>Login To Facebook</h3>
+                <form>
+                    <center>
+                        <input type="email" placeholder="Email Address" />
+                    </center>
+                    <center>
+                        <input type="password" placeholder="Password" />
+                    </center>
+                    <center>
+                        <button type="submit" className="login-button"> Login</button>
+                    </center>
+                    <center className="last-info">
+                        <h5>Forgotten Password ?</h5>
+                        <h5 className="dot">.</h5>
+                        <Link to="/register" style={{textDecoration: 'none'}}>
+                            <h5 className="sign-up">Sign Up for Facebook</h5>
+                        </Link>
+                    </center>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
